f5dd9051ef11c5677c669e716547cf53590f3008
Manda104 <motoko104@yahoo.com>
2018-09-25 20:20:50 -0500

    added email verification and password reset functions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,24 +1,27 @@
- // Initialize Firebase
-  var config = {
+// Initialize Firebase
+var config = {
     apiKey: "AIzaSyDVrSkm_KDrlSCH0KDWo3pQIKfW5xRRWJk",
     authDomain: "cravings-e8497.firebaseapp.com",
     databaseURL: "https://cravings-e8497.firebaseio.com",
     projectId: "cravings-e8497",
     storageBucket: "cravings-e8497.appspot.com",
     messagingSenderId: "938938502432"
-  };
-  firebase.initializeApp(config);
+};
+firebase.initializeApp(config);
 
 //firebase database reference
 let auth = firebase.auth();
 let dB = firebase.database();
 let user = firebase.auth().currentUser;
 
+//global variables to hold for firebase
+let workingUser = '';
+
 // handles signing in 
 const signIn = () => {
     if (user) {
         auth.signOut();
-    }else{
+    } else {
         let email = $(".email-input").val().trim();
         let password = $(".password-input").val().trim();
         //if email is too short or not correct or input
@@ -32,14 +35,14 @@ const signIn = () => {
             return;
         }
         //starting the sign in process
-        auth.signInWithEmailAndPassword(email, password).cath(function(error){
+        auth.signInWithEmailAndPassword(email, password).cath(function (error) {
             //space for handeling errors
             let errorCode = error.code;
             let errorMsg = error.message;
             //lets user know if password is incorrect
-            if (errorCode === 'auth/wrong-password'){
+            if (errorCode === 'auth/wrong-password') {
                 $(".msg").append("Wrong Password.");
-            }else{
+            } else {
                 $(".msg").append(errorMsg);
             }
         })
@@ -62,18 +65,50 @@ const signUp = () => {
         return;
     }
     //creates a new user with an email and password
-    auth.createUserWithEmailAndPassword(email, password).catch(function(error){
+    auth.createUserWithEmailAndPassword(email, password).catch(function (error) {
         let errorCode = error.code;
         let errorMsg = error.message;
         if (errorCode === "auth/weak-password") {
             $(".msg").append("This password is too weak, please create a stronger password");
-        }else{
+        } else {
             $(".msg").append(errorMsg);
         }
+        emailVerif();
     })
 }
 
 // handels sending an email verification
 const emailVerif = () => {
-    
+    user.sendEmailVerification().then(function() {
+        $(".msg").append("Email verification has been sent!");
+    })
 }
+
+//handels password resets
+const passReset = () => {
+    let email = $(".email-input").val().trim();
+    auth.sendPasswordResetEmail(email).then(function() {
+        $(".msg").append("Password reset email has been sent!");
+    }).catch(function(error) {
+        let errorCode = error.code;
+        let errorMsg = error.message;
+        if (errorCode === "auth/invalid-email") {
+            $(".msg").append("Email is invalid");
+            return;
+        }
+        if (errorCode === "auth/user-not-found") {
+            $(".msg").append("Email/user was not found");
+            return;
+        }
+    })
+}
+//saves recent searches in database for each user
+// will create database branch by username, if not already created, then brings up recent searches. only last 5 searches. 
+const searchSave = () => {
+
+}
+
+$(".signIn").on("click", signIn);
+$(".signUp").on("click", signUp);
+$(".restPass").on("click",passReset);
+$(".search").on("click", searchSave);
